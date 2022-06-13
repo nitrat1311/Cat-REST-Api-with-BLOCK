0200d6418a097b4a0db230611ef12ee0d3ec276d
unknown <romayorg@gmail.com>
2022-06-13 15:36:54 +0300

    dqdq

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,7 +12,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
 
   @override
   Widget build(BuildContext context) {
